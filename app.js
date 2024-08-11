2045f94b82b66f38f63dbbe3d3a527e25031fb00
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-08-12 00:42:16 +0530

    bug fixed

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(express.static("public"));
 
 
 
-app.use("/api",userRouter)
-app.use("/api",postRouter)
+app.use("/",userRouter)
+app.use("/",postRouter)
 
 
 app.use((err, req, res, next) => {
