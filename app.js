234d31a18132e85afba6fe530bc5c6cadaa48d9a
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-08-11 22:02:46 +0530

    Cors Added

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors({
+    origin: 'https://your-frontend-domain.vercel.app',
+  }));
 
 
 
