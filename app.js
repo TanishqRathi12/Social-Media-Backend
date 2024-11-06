03c3f524ae50d87994b503fd3f60e1e511a08e3b
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-11-06 23:23:03 +0530

    error check

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://social-media-front-end-seven.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 200
-}
+// const corsOptions = {
+//     origin: 'https://social-media-front-end-seven.vercel.app',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//     optionsSuccessStatus: 200
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // app.use(cors(
 //     {
